f61f14832c28b0aa4f819358df7704d1d586b9cf
Hunter Edward <Hunter.scheel@outlook.com>
2020-06-18 09:35:58 -0500

    *top 25

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -16,7 +16,7 @@ router.post('/players/:name/:time', (req, res) => {
 
 // GET Route
 router.get('/winners', (req, res) => {
-    const queryText = 'SELECT * FROM winners ORDER BY recordtime ASC LIMIT 20';
+    const queryText = 'SELECT * FROM winners ORDER BY recordtime ASC LIMIT 25';
     pool
         .query(queryText)
         .then((result) => {
