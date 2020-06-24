0aa833db8f59a13dd6e2edfdef5e22d9dc9f079b
Hunter Edward <Hunter.scheel@outlook.com>
2020-06-24 13:42:44 -0500

    test

--- src/Components/Leaderboards/Leaderboards.js
+++ src/Components/Leaderboards/Leaderboards.js
@@ -26,7 +26,7 @@ class LeaderBoards extends React.Component {
                             </thead>
                             <tbody>
                                 {this.getRecords()}
-                                {this.props.records.map((x, i) => <tr key={x.username + i}><th scope='row'>#{i + 1}</th><td>{x.username}</td><td>{((x.min / 10) / 15) * 60}s</td></tr>)}
+                                {this.props.records.map((x, i) => <tr key={x.username + i}><th scope='row'>#{i + 1}</th><td>{x.username}</td><td>{(15 / (x.min / 10)) * 60}s</td></tr>)}
                             </tbody>
                         </table>
                     </div>
